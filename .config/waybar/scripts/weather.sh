a67c1a2648e2e3806e3bff95324e6c8b939cc5be
uuu <uuu@uuuthinkpadyoga-hyprland>
2025-09-29 14:02:12 +0930

    bulk update stuff i neglected to commit properly

--- .config/waybar/scripts/weather.sh
+++ .config/waybar/scripts/weather.sh
@@ -1,45 +1,48 @@
 #! /bin/bash
+while true
+do
+	INXI_OUT=$(inxi -w --output json --output-file print)
 
-INXI_OUT=$(inxi -w --output json --output-file print)
+	WEATHER=$(echo "$INXI_OUT" | jq -r '.[]."000#1#0#Weather"[0]."002#0#2#conditions"')
+	TEMP=$(echo "$INXI_OUT" | jq -r '.[]."000#1#0#Weather"[0]."001#0#2#temperature"' | cut -d'.' -f1)
+	TEMP="${TEMP}°"
 
-WEATHER=$(echo "$INXI_OUT" | jq -r '.[]."000#1#0#Weather"[0]."002#0#2#conditions"')
-TEMP=$(echo "$INXI_OUT" | jq -r '.[]."000#1#0#Weather"[0]."001#0#2#temperature"' | cut -d'.' -f1)
-TEMP="${TEMP}°"
+	if [ -z "$TEMP" ]; then
+	  TEMP="--°"
+	fi
 
-if [ -z "$TEMP" ]; then
-  TEMP="--°"
-fi
+	# check openweathermaps condition codes https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 
-# check openweathermaps condition codes https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
-
-if [[ $WEATHER == *"thunderstorm"* ]]; then
-  echo "storm $TEMP"
-elif [[ $WEATHER == *"drizzle"* ]]; then
-  echo "drzl $TEMP"
-elif [[ $WEATHER == *"rain"* ]]; then
-  echo "rain $TEMP"
-elif [[ $WEATHER == *"snow"* ]]; then
-  echo "snow $TEMP"
-elif [[ $WEATHER == *"mist"* ]]; then
-  echo "mist $TEMP"
-elif [[ $WEATHER == *"smoke"* ]]; then
-  echo "smoke $TEMP"
-elif [[ $WEATHER == *"haze"* ]]; then
-  echo "haze $TEMP"
-elif [[ $WEATHER == *"fog"* ]]; then
-  echo "fog $TEMP"
-elif [[ $WEATHER == *"dust"* ]]; then
-  echo "dust $TEMP"
-elif [[ $WEATHER == *"squall"* ]]; then
-  echo "wind $TEMP"
-elif [[ $WEATHER == *"ash"* ]]; then
-  echo "ash $TEMP"
-elif [[ $WEATHER == *"tornado"* ]]; then
-  echo "tndo $TEMP"
-elif [[ $WEATHER == *"cloud"* ]]; then
-  echo "cloud $TEMP"
-elif [[ $WEATHER == *"clear"* ]]; then
-  echo "clear $TEMP"
-else
-  echo "wthr $TEMP"
-fi
+	if [[ $WEATHER == *"thunderstorm"* ]]; then
+	  echo "storm $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"drizzle"* ]]; then
+	  echo "drzl $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"rain"* ]]; then
+	  echo "rain $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"snow"* ]]; then
+	  echo "snow $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"mist"* ]]; then
+	  echo "mist $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"smoke"* ]]; then
+	  echo "smoke $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"haze"* ]]; then
+	  echo "haze $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"fog"* ]]; then
+	  echo "fog $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"dust"* ]]; then
+	  echo "dust $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"squall"* ]]; then
+	  echo "wind $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"ash"* ]]; then
+	  echo "ash $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"tornado"* ]]; then
+	  echo "tndo $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"cloud"* ]]; then
+	  echo "cloud $TEMP" > /tmp/weather
+	elif [[ $WEATHER == *"clear"* ]]; then
+	  echo "clear $TEMP" > /tmp/weather
+	else
+	  echo "wthr $TEMP" > /tmp/weather
+	fi
+	sleep 900
+done
