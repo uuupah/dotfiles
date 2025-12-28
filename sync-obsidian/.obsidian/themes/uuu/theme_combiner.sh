929d688c70e7ddbaabcb746e9e0ec00e9fff04b8
uuu <uuu@uuuthinkpadyoga-hyprland>
2025-12-28 18:48:05 +1030

    add obsidian theme

---
+++ sync-obsidian/.obsidian/themes/uuu/theme_combiner.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cat $HOME/sync-obsidian/.obsidian/themes/uuu/themecolors.css > $HOME/sync-obsidian/.obsidian/themes/uuu/theme.css
+cat $HOME/sync-obsidian/.obsidian/themes/uuu/themerules.css >> $HOME/sync-obsidian/.obsidian/themes/uuu/theme.css
