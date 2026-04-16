eef173c5b7fafe62c1f299a2f1004758d6efbeab
uuu <uuu@uuuthinkpadyoga-hyprland>
2026-04-16 15:19:16 +0930

    keep massaging issues out of device files

--- set_up_links_for_yoga.sh
+++ set_up_links_for_yoga.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-ln -s ~/.config/niri/device-mbpyoga.kdl ~/.config/niri/device.kdl
-ln -s ~/.config/dunst/dunstdevicembpyoga ~/.config/dunst/dunstdevice
-ln -s ~/.config/alacritty/device-mbpyoga.toml ~/.config/alacritty/device.toml
+ln -s ~/.config/niri/device-yoga.kdl ~/.config/niri/device.kdl
+ln -s ~/.config/dunst/dunstdeviceyoga ~/.config/dunst/dunstdevice
+ln -s ~/.config/alacritty/device-yoga.toml ~/.config/alacritty/device.toml
 ln -s ~/.config/waybar/device-yoga.css ~/.config/waybar/device.css
 ln -s ~/.config/tofi/deviceyoga ~/.config/tofi/device
