f3bcc6d0ca17ae567744ac1425d48821743d93fc
uuu <uuu@uuuthinkpadyoga-hyprland>
2026-04-16 11:55:46 +0930

    do another big cleanup on the dotfiles directory

--- sync-obsidian/.obsidian/themes/uuu/theme_combiner.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cat $HOME/sync-obsidian/.obsidian/themes/uuu/themecolors.css > $HOME/sync-obsidian/.obsidian/themes/uuu/theme.css
-cat $HOME/sync-obsidian/.obsidian/themes/uuu/themerules.css >> $HOME/sync-obsidian/.obsidian/themes/uuu/theme.css
