1843864e0220da2f65f0656ec387399fb1833cb6
uuu <mail@uuupah.com>
2026-04-27 12:39:35 +0930

    move search into one script

--- .config/tofi/scripts/search.sh
+++ .config/tofi/scripts/search.sh
@@ -12,22 +12,29 @@ wikipedia
 youtube"
 
 choice=$(printf "%s\n" "$options" | tofi --prompt="sel: " --print-index=true) || exit 0
+config="$HOME
+Changes to be committed:/.config/tofi/configprompt"
 
 # TODO change this choice to indexes instead of words
 case "$choice" in
   1)
-    $HOME/.config/tofi/scripts/search_maps.sh
+    search_term=$(echo "" | tofi --config $config --prompt-text "googlemaps-search: ") || exit 0
+    librewolf --new-tab "https://google.com/maps/search/${search_term// /+}"
     ;;
   2)
-    $HOME/.config/tofi/scripts/search_recipetineats.sh
+    search_term=$(echo "" | tofi --config $config --prompt-text "recipe-tin-eats-search: ") || exit 0
+    librewolf --new-tab "https://recipetineats.com/?s=${search_term// /+}"
     ;;
   3)
-    $HOME/.config/tofi/scripts/search_sallys.sh
+    search_term=$(echo "" | tofi --config $config --prompt-text "sallys-baking-addiction-search: ") || exit 0
+    librewolf --new-tab "https://sallysbakingaddiction.com/?s=${search_term// /+}"
     ;;
   4)
-    $HOME/.config/tofi/scripts/search_wikipedia.sh
+    search_term=$(echo "" | tofi --config $config --prompt-text "wikipedia-search: " ) || exit 0
+    librewolf --new-tab "https://en.wikipedia.org/wiki/Special:Search?sourceid=Mozilla-search&search=${search_term// /+}"
     ;;
   5)
-    $HOME/.config/tofi/scripts/search_youtube.sh
+    search_term=$(echo "" | tofi --config $config --prompt-text "youtube-search: " ) || exit 0
+    librewolf --new-tab "https://youtube.com/results?search_query=${search_term// /+}"
     ;;
 esac
--- .config/tofi/scripts/search_wikipedia.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep -x tofi >/dev/null; then
-  killall -q tofi
-  exit
-fi
-
-search_term=$(echo "" | tofi --config ~/.config/tofi/configprompt --prompt-text "wikipedia-search: " ) || exit 0
-librewolf --new-tab "https://en.wikipedia.org/wiki/Special:Search?sourceid=Mozilla-search&search=${search_term}"
