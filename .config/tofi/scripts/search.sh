adfaea71e09174701e7fdc8774e7fa7fc8d37d32
uuu <uuu@uuuthinkpadyoga-hyprland>
2026-05-01 22:19:30 +0930

    update niri inputs, waybar wifi and tofi search script

--- .config/tofi/scripts/search.sh
+++ .config/tofi/scripts/search.sh
@@ -5,10 +5,10 @@ if pgrep -x tofi >/dev/null; then
   exit
 fi
 
-options="maps
+options="wikipedia
+maps
 recipe tin eats
 sally's baking addiction
-wikipedia
 youtube"
 
 choice=$(printf "%s\n" "$options" | tofi --prompt="sel: " --print-index=true) || exit 0
@@ -16,23 +16,22 @@ config="$HOME/.config/tofi/configprompt"
 search_prompt=""
 search_url=""
 
-# TODO change this choice to indexes instead of words
 case "$choice" in
   1)
+    search_prompt="wikipedia-search: "
+    search_url="https://en.wikipedia.org/wiki/Special:Search?sourceid=Mozilla-search&search="
+    ;;
+  2)
     search_prompt="googlemaps-search: "
     search_url="https://google.com/maps/search/"
     ;;
-  2)
+  3)
     search_prompt="recipe-tin-eats-search: "
     search_url="https://recipetineats.com/?s="
     ;;
-  3)
+  4)
     search_prompt="sallys-baking-addiction-search: "
     search_url="https://sallysbakingaddiction.com/?s="
-    ;;
-  4)
-    search_prompt="wikipedia-search: "
-    search_url="https://en.wikipedia.org/wiki/Special:Search?sourceid=Mozilla-search&search="
     ;;
   5)
     search_prompt="youtube-search: "
