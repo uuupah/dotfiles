20d4d76c50a5d7ec042f7e5b48146d74b70c6fe9
uuu <uuu@uuuthinkpadyoga-hyprland>
2026-05-12 16:41:25 +0930

    fix musing script and sob that it wasnt working before

--- .config/tofi/scripts/musings.sh
+++ .config/tofi/scripts/musings.sh
@@ -3,6 +3,9 @@
 config="$HOME/.config/tofi/configprompt"
 musing=$(echo "" | tofi --config $config --prompt-text "muse: " ) || exit 0
 datetime=$(date +"%Y-%m-%d %T" )
-if [ -n "$musing" ]; then
-  echo "${datetime} - ${musing}" | tee -a "$HOME/sync-obsidian/lists/musings.md"
-else
+
+if [ ! "$musing" ];then
+  exit
+ fi
+
+echo "${datetime} - ${musing}" | tee -a "$HOME/sync-obsidian/lists/musings.md"
