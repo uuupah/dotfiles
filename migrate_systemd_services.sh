966633d10a139ab1263cd09292baae0faa470dfb
uuu <uuu@uuu.soy>
2026-05-22 18:05:50 +0930

    update script for adding symblinks for user systemd services

--- migrate_systemd_services.sh
+++ migrate_systemd_services.sh
@@ -1,3 +1,14 @@
 #! /usr/bin/env bash
 mkdir -p $HOME/.config/systemd/user
-cp ./systemd-services/* $HOME/.config/systemd/user
+
+rm $HOME/.config/systemd/user/swayidle.service
+rm $HOME/.config/systemd/user/swaybg.service
+rm $HOME/.config/systemd/user/inhibit-idle.service
+
+ln ./systemd-services/inhibit-idle.service $HOME/.config/systemd/user/inhibit-idle.service
+ln ./systemd-services/swaybg.service       $HOME/.config/systemd/user/swaybg.service
+ln ./systemd-services/swayidle.service     $HOME/.config/systemd/user/swayidle.service
+
+systemctl --user add-wants niri.service waybar.service
+systemctl --user add-wants niri.service swaybg.service
+systemctl --user add-wants niri.service swayidle.service
